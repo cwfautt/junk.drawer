23cf45b83ab49181f8ed3314278d2566f6a4774d
Chad Fautt <cwfautt@ucdavis.edu>
2018-05-08 01:06:38 -0700

    updated

--- SNP_Search.py
+++ SNP_Search.py
@@ -1,33 +1,39 @@
-"""This finds which SNPs are in which sequences, given coordinates for the SNPs and the start/end of the sequences"""
-
 import random
 from decimal import getcontext, Decimal
 
-#fill both SNP database and exons with random (chr,coord),(chr,coord,coord) entries, respectively.... FOR TESTING PURPOSES ONLY
-SNPs = []
-exons = []
-while len(SNPs) < 600_000:
-    SNPs.append((random.randint(1,24), random.randint(1,1.5e8))) 
-while len(exons) < 20_000: 
+"""This finds which of (queries) are at least partially in each of (seqs),
+given start & end coordinates of the sequences and queries."""
+
+#fill both query database and seqs with random (chr,coord,coord) entries.... FOR TESTING PURPOSES ONLY
+queries = []
+seqs = []
+while len(queries) < 600_000:
+    coord1 = random.randint(1,1.5e8-500)
+    coord2 = coord1 + random.randint(0,500)
+    queries.append((random.randint(1,24), coord1, coord2)) 
+while len(seqs) < 20_000: 
     coord1 = random.randint(1,(1.5e8-15_000))
     coord2 = coord1 + random.randint(4000,15_000)
-    exons.append((random.randint(1,24), coord1, coord2))
+    seqs.append((random.randint(1,24), coord1, coord2))
 
 """ SORTING AND INDEXING DATA """
 
 chr = {}
-#seperate SNPs by chr
-for i in SNPs:
-    if i[0] in chr:
-        chr[i[0]].append(i[1])
+queryHash = {}
+#seperate queries by chr and add each coordinate as a key to the hash table 'queryHash' with their index in 'queries' as the value
+for i in range(len(queries)):
+    queryHash[queries[i][1]] = i
+    queryHash[queries[i][2]] = i
+    if queries[i][0] in chr:
+        chr[queries[i][0]].append(queries[i][1])
+        chr[queries[i][0]].append(queries[i][2])
     else:
-        chr[i[0]] = [i[1]]
-
-#seperate exons by chr and index them
-e = len(str(len(exons)))
+        chr[queries[i][0]] = [queries[i][1], queries[i][2]]
+#seperate seqs by chr and index them
+e = len(str(len(seqs)))
 getcontext().prec = 18
-indexing = 1/10**e #the indexing of exons is done by turning them into floats. the digits after the decimal point are the index and allow easy discrimination between them and the SNPs later.
-for i in exons:
+indexing = 1/10**e #the indexing of seqs is done by turning them into floats. the digits after the decimal point are the index and allow easy discrimination between them and the queries later.
+for i in seqs:
     if i[0] in chr:
         StartIndexed = Decimal(i[1] + indexing).quantize(Decimal(str(1+(1/10**e))))
         chr[i[0]].append(StartIndexed)
@@ -37,31 +43,33 @@ for i in exons:
         chr[i[0]] = [i[1] + indexing, i[2] + indexing]
     indexing += 1/10**e
 
-#sort SNP and exon coordinates in each chromosome
+#sort query and seq coordinates in each chromosome
 for i in chr:
     chr[i].sort()
 
 """ SEARCH """
 
-staging = {} #temporary staging area for associating SNPs with exons
-matches = {} #holds the exons as keys, and each SNP within it in a list
+staging = {} #temporary staging area for associating queries with seqs
+matches = {} #holds the seqs as keys, and each query within it in a list
 for i in chr:
-    #moves through the sorted array of each chr, associating SNPs and exons
+    #moves through the sorted array of each chr, associating queries and seqs
     for j in chr[i]:
-        #the only things in the array that arent ints are the start/end of exons, indexed as float values (xxxx.04-xxxxx.04 is exon #4)
+        #the only things in the array that arent ints are the start/end of seqs, indexed as float values (xxxx.04-xxxxx.04 is seq #4)
         if type(j) is not int:
             exonIndex = int(str(j).split(".")[1])
-            #if we found the end of a particular exon in chr, move it from staging to matches
-            if exons[exonIndex-1] in staging:
-                matches[exons[exonIndex-1]] = staging[exons[exonIndex-1]]
-                staging.pop(exons[exonIndex-1])
-            #if the exon is not already in staging, we must be at its start point; add it to staging
+            #if we found the end of a particular seq in chr, move it from staging to matches
+            if seqs[exonIndex-1] in staging:
+                matches[seqs[exonIndex-1]] = staging[seqs[exonIndex-1]]
+                staging.pop(seqs[exonIndex-1])
+            #if the seq is not already in staging, we must be at its start point; add it to staging
             else:
-                staging[exons[exonIndex-1]] = []
-        #associate ints with all exons currently in staging
+                staging[seqs[exonIndex-1]] = []
+        #associate ints with all seqs currently in staging
         else:
-            for exon in staging:
-                staging[exon].append(j)
-
+            for seq in staging:
+                #only add if I haven't already associated it
+                if queries[queryHash[j]] not in staging[seq]:
+                    staging[seq].append(queries[queryHash[j]])
+                    
 for i in matches:
-    print(f"sequence {i}: {len(matches[i])} SNPs {matches[i]}")
+    print(f"sequence {i}: {len(matches[i])} hit(s) -> {matches[i]}")
